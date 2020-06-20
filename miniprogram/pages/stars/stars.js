89072cf7bfd31d1a0ca640e053e8d9564195d6f6
[181850036] <[181850036@smail.nju.edu.cn]>
2020-06-21 00:24:37 +0800

    getUserFilmImpl

--- miniprogram/pages/stars/stars.js
+++ miniprogram/pages/stars/stars.js
@@ -37,25 +37,30 @@ onLoad:function(options){
       if(res.data[j].openid==ui.openid)
       Film.push(res.data[j])
     }
-    this.setData({
-      starred:Film
-    })
-   
-
-  })
-  allFilms.get().then(res=>{
-    console.log(res)
-    for(let i=0;i<starred.length;i++)
-    for (let j = 0; j < res.data.length; j++) {
-      if(res.data[j]._id==starred[i].videoId)
-      userFilm.push(res.data[j])
+    // this.setData({
+    //   starred:Film
+    // })
+    for(let k=0;k<Film.length;k++){
+      for (let i = 0; i < 9; i++) {
+        allFilms.skip(i*20).get().then(res=>{
+          for (let j = 0; j < res.data.length; j++) {
+              if(Film[k].videoId==res.data[j]._id)
+              userFilm.push(res.data[j])
+            }
+            // this.setData({
+            //   starred:userFilm
+            // })
+          })
+          
+      }
+    
     }
-    this.setData({
-      finalOne:userFilm
-    })
-   
 
   })
+  console.log("Film",Film)
+ 
+console.log("userFilm",userFilm)
+console.log("starred",starred)
       
   },
 
